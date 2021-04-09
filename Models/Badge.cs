b840dc69e2c292179a8be043f04d15d37f6f91d8
Dentsor <dentsor@dentsor.net>
2021-04-09 17:54:37 +0200

    Add dotnet-format for C# auto-formatting

--- Models/Badge.cs
+++ Models/Badge.cs
@@ -1,4 +1,4 @@
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 
 namespace SpeiderappAPI.Models
--- Models/DBContext.cs
+++ Models/DBContext.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace SpeiderappAPI.Models
