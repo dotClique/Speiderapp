e685bb45f946168be5a55b92880a82ca6a2b8db0
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-04-11 13:41:05 +0200

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace SpeiderappAPI.Models
+{
+    public class User
+    {
+
+        public User(long id, string email, string firstName, string lastName)
+        {
+            Id = id;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public long Id { get; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
