88cebc07721accccfc70cc91a0f498748738073d
birkstoveland <bgstovel@stud.ntnu.no>
2021-04-11 14:42:33 +0200

    add migrations for new db models

---
+++ Migrations/20210411124101_created Tag Taggedwith and Category.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SpeiderappAPI.Migrations
+{
+    public partial class createdTagTaggedwithandCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => new { x.Id, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_Tags_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaggedWiths",
+                columns: table => new
+                {
+                    BadgeId = table.Column<long>(type: "bigint", nullable: false),
+                    TagId = table.Column<long>(type: "bigint", nullable: false),
+                    TagId1 = table.Column<long>(type: "bigint", nullable: false),
+                    TagCategoryId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaggedWiths", x => new { x.BadgeId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_TaggedWiths_BadgeList_BadgeId",
+                        column: x => x.BadgeId,
+                        principalTable: "BadgeList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaggedWiths_Tags_TagId1_TagCategoryId",
+                        columns: x => new { x.TagId1, x.TagCategoryId },
+                        principalTable: "Tags",
+                        principalColumns: new[] { "Id", "CategoryId" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaggedWiths_TagId1_TagCategoryId",
+                table: "TaggedWiths",
+                columns: new[] { "TagId1", "TagCategoryId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_CategoryId",
+                table: "Tags",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaggedWiths");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
