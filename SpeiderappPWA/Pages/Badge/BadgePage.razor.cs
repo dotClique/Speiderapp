2d719befe662c7c2b5e32e4ff45bf0ed195848a1
Ulrik Røsby <ulrik@rosby.no>
2022-02-15 22:43:30 +0100

    feat: add badge page files

---
+++ SpeiderappPWA/Pages/Badge/BadgePage.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SpeiderappPWA.Pages.Badge
+{
+
+    public partial class Badges : ComponentBase
+    {
+        [Parameter] public string BadgeId { get; set; }
+
+    }
+}
