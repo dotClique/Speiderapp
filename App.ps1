2e17a8e2e6afd2bf57cf819d8aba4ec8d77702c7
Lars Mitsem Selbekk <lars@selbekk.com>
2022-02-16 23:49:54 +0100

    Allow running any modules with ./App
    One can run all using no parameters or '-Start All',
    or Database and API using '-Start Database, API'

--- App.ps1
+++ App.ps1
@@ -8,30 +8,41 @@
   Author:        Dentsor
   Creation Date: 2022-01-27
   Last Modified: 2022-01-27
-.PARAMETER Action
-  Specify the desired action to run. If not defined, help is automatically selected.
+.PARAMETER Start
+  Specify the modules to start (comma-separated)
 .PARAMETER Detach
   Run Docker-containers in detached state
+.PARAMETER Action
+  Specify the desired action to run
 .PARAMETER OperatingSystem
   You may specify operating system if you wish, by default it is set to Auto which uses Powershell to select the appropriate verion.
 #>
 [CmdletBinding()]
 param (
-	[Parameter(
+  [Parameter(
+    ParameterSetName = "Start",
+    Mandatory = $false,
+    ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true
+  )]
+  [ValidateSet("All", "Database", "Backend", "Frontend")]
+    [String[]]$Start=@("All"),
+  [Parameter(
+    ParameterSetName = "Actions",
 		Position = 0,
-		Mandatory = $false,
-		ValueFromPipeline = $true,
+		Mandatory = $true,
 		ValueFromPipelineByPropertyName = $true
 		)]
-		[ValidateSet('Help', 'Restore', 'Format', 'Migrate', 'StartDB', 'StartAPI', 'StartPWA', 'ActivateVenv', 'DeactivateVenv')]
+		[ValidateSet("Help", "Restore", "Format", "Migrate", "ActivateVenv", "DeactivateVenv")]
 		[String]$Action="Help",
-	[Parameter(
-		Mandatory = $false,
-		ValueFromPipeline = $true,
-		ValueFromPipelineByPropertyName = $true
-		)]
-		[Switch]$Detach,
-	[Parameter(
+ [Parameter(
+    ParameterSetName = "Start",
+    Mandatory = $false,
+    ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true
+  )]
+    [Switch]$Detach,
+  [Parameter(
 		Mandatory = $false,
 		ValueFromPipeline = $true,
 		ValueFromPipelineByPropertyName = $true
@@ -43,11 +54,14 @@ param (
 ######################
 #     Variables      #
 ######################
+$AllModules = "database", "backend", "frontend"
 if ($OperatingSystem -like "Auto") {
 	if ($IsWindows) { $OperatingSystem = "Windows" }
 	elseif ($IsMac) { $OperatingSystem = "Mac" }
 	elseif ($IsLinux) { $OperatingSystem = "Linux" }
 }
+
+$ModulesToStart = (& {If($Start.Contains("All")) { $AllModules } Else { $Start }}).ToLower()
 
 ######################
 #     Functions      #
@@ -90,25 +104,11 @@ Function ActionMigrate() {
 		docker-compose run --rm backend /bin/sh -c "dotnet tool restore; dotnet ef database update --project SpeiderappAPI"
 	}
 }
-Function ActionStartDB() {
+Function StartModules() {
 	if ($Detach) {
-		docker-compose up --detach database
+		docker-compose up --detach $ModulesToStart
 	} else {
-		docker-compose up database
-	}
-}
-Function ActionStartAPI() {
-	if ($Detach) {
-		docker-compose up --detach backend
-	} else {
-		docker-compose up backend
-	}
-}
-Function ActionStartPWA() {
-	if ($Detach) {
-		docker-compose up --detach frontend
-	} else {
-		docker-compose up frontend
+		docker-compose up $ModulesToStart
 	}
 }
 Function ActionActivateVenv() {
@@ -126,15 +126,16 @@ Function ActionDeactivateVenv() {
 ######################
 #     The Script     #
 ######################
-Switch ($Action)
-{
-	"Help" { ActionHelp }
-	"Restore" { ActionRestore }
-	"Format"  { ActionFormat }
-	"Migrate" { ActionMigrate }
-	"StartDB" { ActionStartDB }
-	"StartAPI" { ActionStartAPI }
-	"StartPWA" { ActionStartPWA }
-	"ActivateVenv" { ActionActivateVenv }
-	"DeactivateVenv" { ActionDeactivateVenv }
+Switch ($PsCmdlet.ParameterSetName) {
+  "Actions" {
+    Switch ($Action) {
+      "Help" { ActionHelp }
+      "Restore" { ActionRestore }
+      "Format"  { ActionFormat }
+      "Migrate" { ActionMigrate }
+      "ActivateVenv" { ActionActivateVenv }
+      "DeactivateVenv" { ActionDeactivateVenv }
+    }
+  }
+  "Start" { StartModules }
 }
