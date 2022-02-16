51aabd30045eebffa41ff2bd284f08f81edf01bb
Ulrik Røsby <ulrik@rosby.no>
2022-02-16 11:45:47 +0100

    feat: add basic single badge view

--- SpeiderappPWA/Pages/Badge/Badge.razor.cs
+++ SpeiderappPWA/Pages/Badge/Badge.razor.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace SpeiderappPWA.Pages.Badge
 {
-    public class Badges : ComponentBase
+    public partial class Badge : ComponentBase
     {
+        [Parameter]
+        public int Id { get; set; }
 
+        [Inject]
+        private HttpClient Http {get; set; }
+        private Models.Badge _thisBadge;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var badges = await Http.GetFromJsonAsync<Models.Badge[]>("sample-data/badges.json");
+
+            if (badges == null) return; // TODO: Handle this error
+            _thisBadge = badges.ToList().Find(badge => badge.Id == Id);
+        }
     }
 }
